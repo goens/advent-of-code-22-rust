800ecb86d06970f37634c42a371c765807fe3c7e
Andres Goens <andres.goens@ed.ac.uk>
2022-12-02 14:05:47 +0000

    Split off day1

---
+++ src/day1.rs
@@ -0,0 +1,36 @@
+use std::io::{BufReader, Lines};
+use std::fs::File;
+
+
+pub fn run(lines : Lines<BufReader<File>>) {
+    let mut elves = Vec::new();
+    let mut elf = Vec::new();
+    for (_index, errLine) in lines.enumerate() {
+        if let Ok(line) = errLine{
+            if line.is_empty(){
+                elves.push(elf);
+                elf = Vec::new();
+            }
+            else{
+              let i = match line.parse::<i32>(){
+                  Ok(i) => i,
+                  Err(e) => panic!("couldn't parse int; {}", e),
+              };
+              elf.push(i);
+            }
+        }
+    }
+    let mut total_cals = Vec::new();
+    for elf in elves{
+        let mut cals = 0;
+        for c in elf{
+            cals += c;
+        }
+        total_cals.push(cals);
+    }
+    total_cals.sort();
+    let max_cals = total_cals.pop().unwrap();
+    let three_max = max_cals + total_cals.pop().unwrap() + total_cals.pop().unwrap();
+    println!("max cals: {}", max_cals);
+    println!("top 3 max cals: {}", three_max);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,6 @@
-use std::io::{BufReader, BufRead, Lines};
+use std::io::{BufReader, BufRead};
 use std::fs::File;
-
-fn day1(lines : Lines<?>) {
-    let mut elves = Vec::new();
-    let mut elf = Vec::new();
-    for (_index, errLine) in lines.enumerate() {
-        if let Ok(line) = errLine{
-            if line.is_empty(){
-                elves.push(elf);
-                elf = Vec::new();
-            }
-            else{
-              let i = match line.parse::<i32>(){
-                  Ok(i) => i,
-                  Err(e) => panic!("couldn't parse int; {}", e),
-              };
-              elf.push(i);
-            }
-        }
-    }
-    let mut total_cals = Vec::new();
-    for elf in elves{
-        let mut cals = 0;
-        for c in elf{
-            cals += c;
-        }
-        total_cals.push(cals);
-    }
-    total_cals.sort();
-    let max_cals = total_cals.pop().unwrap();
-    let three_max = max_cals + total_cals.pop().unwrap() + total_cals.pop().unwrap();
-    println!("max cals: {}", max_cals);
-    println!("top 3 max cals: {}", three_max);
-}
+mod day1;
 
 fn main() {
     let file = match File::open("inputs/day1.txt"){
@@ -40,5 +8,5 @@ fn main() {
         Ok(f) => f,
     };
     let reader = BufReader::new(file);
-    day1(reader.lines());
+    day1::run(reader.lines());
 }
